4d529d36dc8772dd99a1d922666f1b91d37c58a2
Pavel Lang <poistovak@gmail.com>
2026-07-31 06:23:33 +0200

    refactor(package): add heos_ui root package

---
+++ packages/heos_ui/__init__.py
@@ -0,0 +1,11 @@
+"""
+HEOS UI
+"""
+
+__version__ = "0.1.0"
+
+from . import components
+
+__all__ = [
+    "components",
+]
---
+++ packages/heos_ui/components/__init__.py
@@ -0,0 +1,31 @@
+"""
+HEOS UI Components
+"""
+
+from packages.components import (
+    Badge,
+    BatteryIndicator,
+    BatteryState,
+    Button,
+    Card,
+    DashboardLayout,
+    EnergyUnit,
+    EnergyValue,
+    PowerFlowIndicator,
+    PowerNode,
+    StatusChip,
+)
+
+__all__ = [
+    "Badge",
+    "BatteryIndicator",
+    "BatteryState",
+    "Button",
+    "Card",
+    "DashboardLayout",
+    "EnergyUnit",
+    "EnergyValue",
+    "PowerFlowIndicator",
+    "PowerNode",
+    "StatusChip",
+]
